a7f7c38fda918167ae7e9d3226bcfeecd8e78733
aotianlong <aotianlong@hengfirst.com>
2023-03-27 22:37:09 +0800

    尝试使用postmessage通信g

--- src/mbm/index.ts
+++ src/mbm/index.ts
@@ -1,7 +1,15 @@
 import { useUserStore } from './store'
 
 const userStore = useUserStore()
-window.addEventListener('message', (data) => {
-	console.log('on message', data);
-	userStore.updateUserInfo({name: 'aotianlong'});
+/*
+ * data: {
+ * 		action: 'action',
+ * 		data: {somedata: ''}
+ *	}
+ */
+window.addEventListener('message', (event) => {
+	const { data } = event
+	if (data.action === 'UpdateUser') {
+		userStore.updateUserInfo(data.data);
+	}
 })
