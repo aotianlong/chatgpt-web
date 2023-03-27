33f7fc4caad750e095028c6c6b457893fc74bb2f
aotianlong <aotianlong@hengfirst.com>
2023-03-27 19:25:58 +0800

    save

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig((env) => {
   const viteEnv = loadEnv(env.mode, process.cwd()) as unknown as ImportMetaEnv
 
   return {
-    base: '/musicgpt-chat',
+    base: '',
     resolve: {
       alias: {
         '@': path.resolve(process.cwd(), 'src'),
