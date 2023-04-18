880803b24e7f25d3280b5556f4d0877295da9d0f
aotianlong <aotianlong@gmail.com>
2023-04-18 22:49:14 +0800

    feat: 可以通过数据验证用户有效性，让用户必须访问musicgpt.com才能使用

--- service/src/middleware/auth-with-db.ts
+++ service/src/middleware/auth-with-db.ts
@@ -1,32 +1,37 @@
 import { PrismaClient } from '@prisma/client'
-import { isNotEmptyString } from '../utils/is'
+import _ from 'lodash'
 
 const prisma = new PrismaClient()
-prisma.sessions
-  .findFirst({ where: { token: '1' } })
-  .then(session => console.log(session))
 
-const auth = async (req, res, next) => {
-  const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY
-  if (isNotEmptyString(AUTH_SECRET_KEY)) {
-    try {
-      const Authorization = req.header('Authorization')
-      const token = Authorization && Authorization.replace('Bearer ', '').trim()
-      if (token) {
-        const session = await prisma.sessions
-          .findFirst({ where: { token } })
-        if (session)
-          return next()
-      }
-      throw new Error('Error: 无访问权限 | No access rights')
-    }
-    catch (error) {
-      res.send({ status: 'Unauthorized', message: error.message ?? 'Please authenticate.', data: null })
-    }
-  }
-  else {
-    next()
-  }
+const getSession = (req) => {
+	let token = req
+	if (!_.isString(req)) {
+	  const Authorization = req.header('Authorization')
+	  token = Authorization && Authorization.replace('Bearer ', '').trim()
+	}
+	console.log(token)
+	if (token) {
+		const session = prisma.sessions.findFirst({ where: { token } })
+		return session
+	} else {
+		return Promise.resolve(null)
+	}
 }
 
-export { auth }
+const auth = async (req, res, next) => {
+	const session = await getSession(req)
+	console.log('session', session)
+
+	try {
+		if (!session) {
+			throw new Error('Error: 无访问权限 | No access rights')
+		} else {
+			next()
+		}
+	}
+	catch (error) {
+		res.send({ status: 'Unauthorized', message: error.message ?? 'Please authenticate.', data: null })
+	}
+}
+
+export { auth, getSession }
